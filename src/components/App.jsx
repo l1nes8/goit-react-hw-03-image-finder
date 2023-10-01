af68bb8c1aca2653360c7586a9c49d9e95bc7c4f
l1nes8 <egor1glavatyje@gmail.com>
2023-10-01 13:55:52 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,16 +26,22 @@ export class App extends Component {
     const apiKey = '38965444-221e39e59f698a8ee4d2c4c8b';
     const url = `https://pixabay.com/api/?q=${query}&page=${page}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=${perPage}`;
 
-    this.setState({ isLoading: true });
+    this.setState({ isLoading: true, error: '' });
 
     axios
       .get(url)
       .then(response => {
-        this.setState(prevState => ({
-          images: [...prevState.images, ...response.data.hits],
-          page: prevState.page + 1,
-          totalHits: response.data.totalHits,
-        }));
+        const { hits } = response.data;
+
+        if (hits.length === 0) {
+          this.setState({ error: "We don't have any photos for your request" });
+        } else {
+          this.setState(prevState => ({
+            images: [...prevState.images, ...response.data.hits],
+            page: prevState.page + 1,
+            totalHits: response.data.totalHits,
+          }));
+        }
       })
       .catch(error => {
         console.error('Error fetching images:', error);
@@ -99,7 +105,7 @@ export class App extends Component {
             onClose={this.handleCloseModal}
           />
         )}
-        {error && <p className="error">{error}</p>}
+        {error && <h1 className={css.error}>{error}</h1>}
       </div>
     );
   }
